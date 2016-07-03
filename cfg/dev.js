761647da911da354192b299084761885d9d1f6d9
iQoderi <841599872@qq.com>
2016-07-03 09:55:25 +0800

    Update dev.js

--- cfg/dev.js
+++ cfg/dev.js
@@ -15,7 +15,7 @@ let config = Object.assign({}, baseConfig, {
     './src/index'
   ],
   cache: true,
-  devtool: 'eval-source-map',
+  devtool: false,
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
