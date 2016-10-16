b6fd9f7ee1848581dd54081e5f30c7182c3c3165
iQoderi <841599872@qq.com>
2016-10-17 00:03:58 +0800

    Update secrt.js

--- secrt.js
+++ secrt.js
@@ -4,18 +4,18 @@
  */
 const secrt = {
   qiniu: {
-    ak: 'aqU04gUOGu9yMX6R6KS33rDLSlrrxoa4XKByqAaw',
-    sk: 'g3br-F3D53huzCVD4jkFhkID0HZaX3MJjiSqkF58',
-    bucket: 'neuqst',
-    url: 'http://odljp7x9v.bkt.clouddn.com/',
-    dir: 'assets/'
+    ak: 'xxx',
+    sk: 'xxx',
+    bucket: 'xxx',
+    url: 'xxx',
+    dir: 'xxx'
   },
   remoteServer: {
-    host: '123.206.16.40',
-    // port: 22,
-    user: 'ubuntu',
-    pass: 'Qoder5143209',
-    remoteDir: '/var/www/NEUQSXTJGRADUATE',
+    host: 'xxx',
+    // port: 'xx',
+    user: 'xxx',
+    pass: 'xxx',
+    remoteDir: 'xxx',
   }
 };
 
