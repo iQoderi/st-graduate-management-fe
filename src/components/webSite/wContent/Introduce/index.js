a231dabcaee09f93cff19b6f4779646f4dfdc56b
qoder <qoder@hrsoft.net>
2016-10-20 11:55:36 +0800

    平台简介完成

--- src/components/webSite/wContent/Introduce/index.js
+++ src/components/webSite/wContent/Introduce/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import UnderlineTitle from '../../underlineTitle';
+import IntroWord from './introduceWord';
+import UseIntro from './useIntro';
 import './index.css';
 
-const Introduce = (props)=> {
+const Introduce = ()=> {
   return (
-    <div>平台简介</div>
+    <div className="w-intro-wrapper">
+      <UnderlineTitle title="平台简介"/>
+      <IntroWord/>
+      <UnderlineTitle title="使用说明"/>
+      <UseIntro/>
+    </div>
   )
 };
 
